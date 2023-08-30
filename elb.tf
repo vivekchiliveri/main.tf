b7710357b02540b943932bd978258b868f345eba
vivekchiliveri <136873306+vivekchiliveri@users.noreply.github.com>
2023-08-30 08:11:27 +0530

    Create elb.tf

---
+++ elb.tf
@@ -0,0 +1,48 @@
+resource "aws_elb" "bar" {
+  name               = "raham-terraform-elb"
+  availability_zones = ["us-east-1a", "us-east-1b"]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 3
+    unhealthy_threshold = 5
+    timeout             = 5
+    target              = "HTTP:80/"
+    interval            = 30
+  }
+
+  instances                 = ["${aws_instance.one.id}", "${aws_instance.two.id}"]
+  cross_zone_load_balancing = true
+  idle_timeout              = 400
+  tags = {
+    Name = "raham-tf-elb"
+  }
+}
+
+ /*resource "aws_rds_cluster" "default" {
+  cluster_identifier      = "aurora-cluster-demo"
+  engine                  = "aurora-mysql"
+  engine_version          = "5.7.mysql_aurora.2.11.2"
+  availability_zones      = ["eu-west-3a", "eu-west-3b"]
+  database_name           = "mydb"
+  master_username         = "vivek"
+  master_password         = "vivekchiliveri#1122"
+  backup_retention_period = 5
+  preferred_backup_window = "07:00-09:00"
+}*/
+
+/*resource "aws_instance" "one" {
+  for_each = toset(["one", "two", "three"])
+  ami           = "ami-0f409bae3775dc8e5"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "instance-${each.key}"
+  }
+}
+*/
